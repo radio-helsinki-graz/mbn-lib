2175c3f39c0c57e755bcd725a601fbe5b6a364e4
Yoran Heling <projects@yorhel.nl>
2009-03-02 02:44:21 +0100

    Let's not forget the copyright notice in each file

--- src/codec.c
+++ src/codec.c
@@ -1,3 +1,18 @@
+/****************************************************************************
+**
+** Copyright (C) 2009 D&R Electronica Weesp B.V. All rights reserved.
+**
+** This file is part of the Axum/MambaNet digital mixing system.
+**
+** This file may be used under the terms of the GNU General Public
+** License version 2.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of
+** this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
 
 #include <stdlib.h>
 #include <string.h>
