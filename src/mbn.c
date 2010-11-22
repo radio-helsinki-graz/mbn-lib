dce3cf142adf980a216e453bec8b4574ad48b5dc
Anton Prins <a.prins@d-r.nl>
2010-11-22 11:55:56 +0100

    Version changed to v1.9

--- src/mbn.c
+++ src/mbn.c
@@ -537,7 +537,7 @@ void MBN_EXPORT mbnWriteLogMessage(struct mbn_interface *itf, const char *fmt, .
 }
 
 const char *MBN_EXPORT mbnVersion() {
-  sprintf(versionString, "MambaNet Library V1.9 Beta - %s (%s) - %s", __DATE__, __TIME__, MBN_MANUFACTURER_NAME);
+  sprintf(versionString, "MambaNet Library V1.9 - %s (%s) - %s", __DATE__, __TIME__, MBN_MANUFACTURER_NAME);
   return versionString;
 }
 
